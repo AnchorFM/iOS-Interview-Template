9ef228dfafa662859e3e0d6d4ce0f67f5f436aee
Saturnino Collaço <saturninoc@spotify.com>
2021-04-06 17:58:20 -0400

    Remove example data source

--- AnchorSampleProject/Controllers/ViewController.swift
+++ AnchorSampleProject/Controllers/ViewController.swift
@@ -5,7 +5,6 @@ final class ViewController: UIViewController {
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.dataSource = self
         tableView.register(
             AlbumTableViewCell.self,
             forCellReuseIdentifier: AlbumTableViewCell.defaultIdentifier
@@ -24,20 +23,4 @@ final class ViewController: UIViewController {
         view.addSubview(tableView)
         tableView.fitToEdges(of: view)
     }
-}
-
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: AlbumTableViewCell.defaultIdentifier) else {
-            return UITableViewCell()
-        }
-
-        return cell
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-
-
 }
